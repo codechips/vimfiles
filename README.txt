020baab38f8a128f41fc54c064fdfab5e7f0945c
Ilia Mikhailov <ilia.mikhailov@viaplay.com>
2015-01-26 16:31:57 +0100

    Fix setup; Add readme

---
+++ README.txt
@@ -0,0 +1,7 @@
+## JAVS - Just Another Vim Setup
+
+## note to self
+
+To install vim config on another machine run:
+
+`ssh user@192.168.0.10 'bash -s' < setup.bash`
--- setup.bash
+++ setup.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 echo Setting up vim env ..
 git clone https://github.com/iljoo/vimfiles.git  ~/.vim
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
